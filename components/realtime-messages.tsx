836a51029e08372867619b878c47e2158c85b5b3
prateekvarma <prtk.varma@gmail.com>
2023-06-18 16:18:28 -0600

    Subscribed to real time messages

--- components/realtime-messages.tsx
+++ components/realtime-messages.tsx
@@ -1,4 +1,7 @@
+import { useOutletContext } from '@remix-run/react';
 import { Database } from 'db_types';
+import { useEffect, useState } from 'react';
+import { SupabaseOutletContext } from '~/root';
 
 type Message = Database['public']['Tables']['messages']['Row'];
 
@@ -7,5 +10,36 @@ export default function RealtimeMessages({
 }: {
   serverMessages: Message[];
 }) {
-  return <pre>{JSON.stringify(serverMessages, null, 2)}</pre>;
+  const [messages, setMessages] = useState(serverMessages);
+  const { supabase } = useOutletContext<SupabaseOutletContext>();
+
+  //this 2nd useEffect is for when user logs in/out -- which is when our 'serverMessages' change
+  useEffect(() => {
+    setMessages(serverMessages);
+  }, [serverMessages]);
+
+  useEffect(() => {
+    const channel = supabase
+      .channel('*')
+      .on(
+        'postgres_changes',
+        { event: 'INSERT', schema: 'public', table: 'messages' },
+        (payload) => {
+          // gets the new inserted message
+          const newMessage = payload.new as Message;
+
+          //check if message is already in our state, if not, insert it
+          if (!messages.find((message) => message.id === newMessage.id)) {
+            setMessages([...messages, newMessage]);
+          }
+        }
+      )
+      .subscribe();
+
+    return () => {
+      supabase.removeChannel(channel);
+    };
+  }, [supabase, messages, setMessages]);
+
+  return <pre>{JSON.stringify(messages, null, 2)}</pre>;
 }
